b3a742142cf7cae55aa906f3ec3bb80104a9c10f
Joaquin Mayor <joaquin88mayor@gmail.com>
2026-09-25 12:17:28 -0300

    fix texts

--- src/main/java/com/reactive/api/flux/reactive/strams/tractor_api_flux_reactive_strams/TractorApiFluxReactiveStramsApplication.java
+++ src/main/java/com/reactive/api/flux/reactive/strams/tractor_api_flux_reactive_strams/TractorApiFluxReactiveStramsApplication.java
@@ -35,19 +35,19 @@ public class TractorApiFluxReactiveStramsApplication implements CommandLineRunne
 		Flux<String> nombresUsuariosList = Flux.fromIterable(usuariosList); //Crear un flux a partir de una lista
 		
 		//Recordar que para trabajar con flux se debe agregar en el pom las dependencias de Reactor
-		Flux<Usuario> nombres = Flux.just("Andres Guzman", "Pedro Fulano", "Diego Sultano","Juan Mengano","Maria Fulana", "Bruce Lee","Bruce Willis")//Creamos obserbables con el flux y su metodo just, donde se le pasa la data
+		Flux<Usuario> nombres = Flux.just("Andres Guzman", "Pedro Fulano", "Diego Sultano","Juan Mengano","Maria Fulana", "Bruce Lee","Bruce Willis")//Creamos observables con el flux y su método just, donde se le pasa la data
 		.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
 		.filter(usuario -> usuario.getName().equalsIgnoreCase("Bruce")) //Filtramos por el nombre bruce
 		.doOnNext(element ->{
 			if(element == null){
-				throw new RuntimeException("Nombres no se pueden vacios"); //manejo de errores dentro del doOnNext, esto termina el flujo
+				throw new RuntimeException("Nombres no pueden ser vacíos"); //manejo de errores dentro del doOnNext, esto termina el flujo
 			}
 				System.out.println(element.getName().concat(" ").concat(element.getLastname()));
-			})//Esto es lo que va a hacer cuando nos subscribamos, notifica ccuanod haya llegado un elemento
+			})//Esto es lo que va a hacer cuando nos suscribamos, notifica cuando haya llegado un elemento
 			.map(usuario ->{
 				String nombre = usuario.getName().toLowerCase();
 				usuario.setName(nombre);
-				return usuario;}); //Retornamos otra instancia del flux con los datos cambiados, el map sirve para generar otra instancia modificada, la posición del map afecta a la ejecución si se pone antes se mostraran en mayusculas , si se pone despues no, afectando solo a los métodos del subscribe
+				return usuario;}); //Retornamos otra instancia del flux con los datos cambiados, el map sirve para generar otra instancia modificada, la posición del map afecta a la ejecución si se pone antes se mostrarán en mayúsculas , si se pone después no, afectando solo a los métodos del subscribe
 			
 
 		nombres.subscribe(e-> log.info(e.getName()), error -> log.error(error.getMessage()), new Runnable() {
@@ -60,7 +60,7 @@ public class TractorApiFluxReactiveStramsApplication implements CommandLineRunne
 			
 		}); //El subscribe no es solo para aplicar sino que puede hacer modificaciones en el método
 		                 //Con el log hacemos que se imprima en el log cuando se hace el doOnNext
-		//En los subcribe se pueden pasar 3 parametros, el codigo que queremos que se ejecute, el codigo del error y el codigo de cuando termina de ejecutarse, con el Runnable que es una interface
+		//En los subscribe se pueden pasar 3 parámetros, el código que queremos que se ejecute, el código del error y el código de cuando termina de ejecutarse, con el Runnable que es una interface
 	}
 		
 
